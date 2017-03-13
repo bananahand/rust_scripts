44eac490b1c40b41773039a3e0897bdbc43897b3
bananahand <nick@nobullcraft.com>
2017-03-13 12:09:04 -0400

    Adding locking to prevent collisions.

--- version.sh
+++ version.sh
@@ -3,11 +3,20 @@
 currnet_version=$(grep -R "buildid" /home/rustserver/serverfiles/steamapps/appmanifest_258550.acf | awk '{print $2}' | sed "s/\"//g")
 public_version=$(curl -s https://steamdb.info/app/258550/depots/?branch=public | grep -o "<i>buildid:</i> <b>[0-9]*</b>" | grep -o [0-9]*)
 
+set -e
+
+( flock -n 9001
+
 if [ $currnet_version == $public_version ]
 then
+	echo sleep10
+	sleep 10
+	echo done
 	exit 0
 else
 	/home/rustserver/rustserver stop
 	/home/rustserver/rustserver update
 	/home/rustserver/rustserver monitor
 fi
+
+) 9001>/tmp/rust_version_check.lock
