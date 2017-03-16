b115ed07d57e0696139ee8471f71a59949b46f4b
bananahand <nick@nobullcraft.com>
2017-03-16 17:29:17 -0400

    Making version.sh better

--- version.sh
+++ version.sh
@@ -1,19 +1,6 @@
 #!/bin/bash
 
-currnet_version=$(grep -R "buildid" /home/rustserver/serverfiles/steamapps/appmanifest_258550.acf | awk '{print $2}' | sed "s/\"//g")
-public_version=$(curl -s https://steamdb.info/app/258550/depots/?branch=public | grep -o "<i>buildid:</i> <b>[0-9]*</b>" | grep -o [0-9]*)
-
-set -e
-
-( flock -n 9001
-
-if [ $currnet_version == $public_version ]
-then
-	exit 0
-else
-	/home/rustserver/rustserver stop
-	/home/rustserver/rustserver update
-	/home/rustserver/rustserver monitor
-fi
-
+set -e 
+( flock -n 9001 
+/bin/bash /home/rustserver/rustserver update 
 ) 9001>/tmp/rust_version_check.lock
